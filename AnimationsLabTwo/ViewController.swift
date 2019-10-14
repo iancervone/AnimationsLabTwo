ad03d856669f6ac0c0881c3c940764c5c32d9024
Ian <ian@Ians-MacBook-Pro.local>
2019-10-14 16:43:30 -0400

    first commit

---
+++ AnimationsLabTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationsLabTwo
+//
+//  Created by Ian Cervone on 10/14/19.
+//  Copyright © 2019 Ian Cervone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
